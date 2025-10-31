f99fe8f545a95bcf674168018d0386b014e5ade1
Maciej Sala <maciek.a.sala@gmail.com>
2025-10-31 14:24:44 +0100

    refactor(layout): add cleaner code

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,20 +1,32 @@
 import type { Metadata } from "next";
-import type { ReactNode } from "react";
+
 import "./globals.css";
 import { MarginLayout } from "../components/layout/MarginLayout";
 import ClientProviders from "@/components/ClientProviders";
+import { Header } from "@/components/layout/Header";
+import { Locale } from "@/lib/i18n/dictionaries";
 
 export const metadata: Metadata = {
   title: "Warhammer Army Builder",
   description: "Narzędzie do tworzenia rozpiski armii Warhammer.",
 };
 
-export default function RootLayout({ children }: { children: ReactNode }) {
+export default async function RootLayout({
+  params,
+  children,
+}: {
+  params: { locale: Locale; slug?: string[] };
+  children: React.ReactNode;
+}) {
+  const restSegments: string[] = params.slug ?? [];
   return (
-    <html lang="pl">
+    <html lang={params.locale}>
       <body className="bg-slate-600 text-amber-300">
         <ClientProviders>
-          <MarginLayout>{children}</MarginLayout>
+          <Header locale={params.locale} restSegments={restSegments} />
+          <main className="pt-20">
+            <MarginLayout>{children}</MarginLayout>
+          </main>
         </ClientProviders>
       </body>
     </html>
